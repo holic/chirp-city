18d5e1273e16bd89588f5f65836fd20458560d16
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-15 20:43:09 -0800

    add tailwindcss

---
+++ packages/app/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Tweets on chain</title>
+      </Head>
+
+      <div>hello world</div>
+    </>
+  );
+};
+
+export default Home;

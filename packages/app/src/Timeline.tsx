b49c8a98af5123ba0b7eb8719770531cc1fe2b90
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-17 01:07:15 -0800

    detect chain and switch

--- packages/app/src/Timeline.tsx
+++ packages/app/src/Timeline.tsx
@@ -9,10 +9,14 @@ import { RelativeTime } from "./RelativeTime";
 import { useTimeline } from "./useTimeline";
 import { useWallet } from "./useWallet";
 
+const chainId = 80001;
+
 export const Timeline = () => {
   const { account, provider, connect } = useWallet();
   const tweets = useTimeline();
   const [message, setMessage] = useState("");
+
+  const isWrongChain = provider && provider.network.chainId !== chainId;
 
   return (
     <div className="flex flex-col flex-wrap divide-y border">
@@ -22,6 +26,15 @@ export const Timeline = () => {
 
           if (!provider) {
             connect();
+            return;
+          }
+
+          if (isWrongChain) {
+            // TODO: offer to add chain config if this fails
+            // https://github.com/kesne/ethereal-react/blob/c1645b231f741df5dde28d87c91e8937c8a9443f/packages/ethereal-react/src/network.tsx#L62-L75
+            provider.send("wallet_switchEthereumChain", [
+              { chainId: `0x${chainId.toString(16)}` },
+            ]);
             return;
           }
 
@@ -39,20 +52,24 @@ export const Timeline = () => {
           console.log("tweet sent");
         }}
       >
-        <div className="p-4 flex gap-4">
-          {account ? <AccountAvatar address={account} /> : <Avatar />}
-          <div className="flex flex-col flex-wrap w-full">
-            <div className="flex text-xl py-2">
-              <textarea
-                className="w-full h-16 outline-none resize-none"
-                placeholder="What's happening?"
-                value={message}
-                onChange={(event) => {
-                  setMessage(event.currentTarget.value);
-                }}
-                required
-              />
+        <div className="flex flex-col gap-4 py-4">
+          <div className="px-4 flex gap-4">
+            {account ? <AccountAvatar address={account} /> : <Avatar />}
+            <div className="flex flex-col flex-wrap w-full">
+              <div className="flex text-xl py-2">
+                <textarea
+                  className="w-full h-16 outline-none resize-none"
+                  placeholder="What's happening?"
+                  value={message}
+                  onChange={(event) => {
+                    setMessage(event.currentTarget.value);
+                  }}
+                  required
+                />
+              </div>
             </div>
+          </div>
+          <div className="px-4 flex gap-4 justify-between items-center flex-row-reverse">
             <button
               type="submit"
               className="self-end rounded-full bg-blue-500 px-4 py-2 font-bold text-white transition disabled:opacity-60 disabled:hover:bg-blue-500 hover:bg-blue-600 active:bg-blue-700"
@@ -60,6 +77,11 @@ export const Timeline = () => {
             >
               Tweet
             </button>
+            {isWrongChain && message ? (
+              <div className="bg-yellow-300 px-2 py-1">
+                Please switch to Polygon testnet (mumbai)
+              </div>
+            ) : null}
           </div>
         </div>
       </form>

f4db6f83a323c87c5115482089f7a8e43219ed35
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-16 22:50:03 -0800

    default to dicebear avatar

--- packages/app/src/Avatar.tsx
+++ packages/app/src/Avatar.tsx
@@ -9,13 +9,18 @@ export const Avatar = ({ address }: Props) => {
 
   return (
     <div className="flex-shrink-0 w-16 h-16 rounded-full overflow-hidden bg-gray-200">
-      {avatar ? (
-        <img
-          src={avatar}
-          alt={`avatar for ${name || address}`}
-          className="w-full h-full object-cover object-center"
-        />
-      ) : null}
+      {/* TODO: figure out ENS avatar returns ipfs, if so we probably can't use Next.js' `Image` component */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={
+          avatar ||
+          `https://avatars.dicebear.com/api/croodles-neutral/${encodeURIComponent(
+            address
+          )}.svg`
+        }
+        alt={`avatar for ${name || address}`}
+        className="w-full h-full object-cover object-center"
+      />
     </div>
   );
 };

ce54010315cfebfdefbf8090d3297024e974b969
Kevin Ingersoll <kingersoll@gmail.com>
2022-01-01 19:01:36 -0800

    add subgraph

---
+++ packages/subgraph/src/mapping.ts
@@ -0,0 +1,16 @@
+import { ChirpCityMessage } from "../generated/ChirpCity/ChirpCity";
+import { Message } from "../generated/schema";
+
+export function handleChirpCityMessage(event: ChirpCityMessage): void {
+  const id = `${event.block.number}:${event.logIndex}`;
+
+  // Messages are currently immutable so we shouldn't need to load an
+  // existing message by the same ID before setting/saving.
+  const message = new Message(id);
+
+  message.timestamp = event.block.timestamp.toU32();
+  message.from = event.params.from.toHexString();
+  message.message = event.params.message;
+
+  message.save();
+}

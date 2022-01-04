32f590636b521fede61679edcc208c5b29a06767
Kevin Ingersoll <kingersoll@gmail.com>
2022-01-04 00:37:24 -0800

    parse quoted chirps in subgraph

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,3 +1,5 @@
+import { RegExp } from "assemblyscript-regex";
+
 import { ChirpCityMessage } from "../generated/ChirpCity/ChirpCity";
 import { Message } from "../generated/schema";
 
@@ -11,6 +13,19 @@ export function handleChirpCityMessage(event: ChirpCityMessage): void {
   message.timestamp = event.block.timestamp.toU32();
   message.from = event.params.from.toHexString();
   message.message = event.params.message;
+
+  const parents: string[] = [];
+  // assemblyscript-regex doesn't properly handle multiline matches with ^$
+  // so we have to split the string by line and match each line individually.
+  const lines = event.params.message.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    const re = new RegExp("^\\s*>\\s*(chirp:\\d+:\\d+)\\s*$");
+    const match = re.exec(lines[i]);
+    if (match) {
+      parents.push(match.matches[1]);
+    }
+  }
+  message.parents = parents;
 
   message.save();
 }
